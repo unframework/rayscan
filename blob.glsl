0bc610e760e440d777ec130271cc9455690ba6bb
Nick Matantsev <nick.matantsev@gmail.com>
2017-11-15 17:00:56 -0500

    Import code from old Book of Shaders experiment

---
+++ blob.glsl
@@ -0,0 +1,73 @@
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+uniform vec2 u_resolution;
+uniform vec2 u_mouse;
+uniform float u_time;
+
+float distanceToSphere(vec3 pt, vec3 ctr, float radius) {
+    return distance(pt, ctr) - radius;
+}
+
+float smin(float a, float b, float k) {
+    float res = exp(-k * a) + exp(-k * b);
+    return -log(res) / k;
+}
+
+float rawDistanceToObject(vec3 pt) {
+    return smin(
+        distanceToSphere(pt, vec3(-0.500 + 0.05 * sin(u_time * 7.0),0.000,-5.000), 1.5),
+        distanceToSphere(pt, vec3(1.000,0.340,-4.900), 1.2 + 0.05 * sin(u_time * 2.5) * sin(u_time * 17.0)),
+        5.0
+    );
+}
+
+vec3 displacer(vec3 pt) {
+    return 0.0 * vec3(
+        sin(pt.x / 0.3) *
+        sin(pt.y / 0.2) *
+        sin(pt.z / 0.2)
+    );
+}
+
+float distanceToObject(vec3 pt) {
+    return rawDistanceToObject(pt + displacer(pt));
+}
+
+vec3 calculateNormal(in vec3 pt) {
+    vec2 eps = vec2(1.0, -1.0) * 0.0005;
+    
+    return normalize(eps.xyy * distanceToObject(pt + eps.xyy) +
+                     eps.yyx * distanceToObject(pt + eps.yyx) +
+                     eps.yxy * distanceToObject(pt + eps.yxy) +
+                     eps.xxx * distanceToObject(pt + eps.xxx));
+}
+
+void main() {
+    vec2 st = (gl_FragCoord.xy) / u_resolution.xy - vec2(0.5, 0.5);
+    st.x *= u_resolution.x / u_resolution.y;
+    
+    vec3 rayOrigin = vec3(0.0, 0.0, 1.0);
+    vec3 rayDirection = normalize(vec3(st, 0.0) - rayOrigin);
+    
+    float distance;
+    float photonPosition = 1.0;
+    
+    for (int i = 0; i < 100; i += 1) {
+        distance = distanceToObject(rayOrigin + rayDirection * photonPosition);
+        photonPosition += distance;
+        
+        if (distance < 0.01) {
+            break;
+        }
+    }
+    
+    if (distance > 0.01) {
+        gl_FragColor = vec4(vec3(0.140,0.120,0.106), 1.000);
+    } else {
+        vec3 intersectionNormal = calculateNormal(rayOrigin + rayDirection * photonPosition);
+        float amt = -dot(intersectionNormal, normalize(vec3(1.220,-0.300,-0.700)));
+        gl_FragColor = vec4(amt*vec3(0.765,0.196,0.004), 1.000);
+    }
+}
